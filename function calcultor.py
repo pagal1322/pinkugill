de655c27d1267c4c58ae637c79ee650a734bdabc
pinku gill <92435461+pagal1322@users.noreply.github.com>
2021-11-08 14:42:59 +0530

    function cal.py
    function cal.py

---
+++ function calcultor.py
@@ -0,0 +1,27 @@
+# def add(a,b):
+#     add=a+b
+#     return add
+# def sub(a,b):
+#     sub=a-b
+#     return sub
+# def mul(a,b):
+#     mul=a*b
+#     return mul
+# def div(a,b):
+#     div=a/b
+#     return div
+# def main():
+#   symbol=(input("enter symbol"))
+#   if symbol=="+":
+#     print(add(12,5))
+#   elif symbol=="-":
+#     print(sub(12,5))
+#   elif symbol=="*":
+#     print(mul(12,5))
+#   elif symbol=="/":
+#     print(div(12,5))
+# main()
+
+
+
+# Main function........ calculator
