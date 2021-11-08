53c7dd7bb0dfbdded8530bb98809198c7e23eb7a
pinku gill <92435461+pagal1322@users.noreply.github.com>
2021-11-08 14:49:56 +0530

    funct.py
    funct.py

---
+++ funct.py
@@ -0,0 +1,114 @@
+# def odd num(num1,num2):
+#     print("two num ka add")
+#     print(num1+num2:)
+# odd-num(120,50)
+# max=str(134)
+# name="pinku"
+# odd-num(max,name)
+
+# def add(a,b):
+# 	c=a+b
+# 	print(c)
+# return(add(6,7))
+
+
+# def example (num):
+# 	if (num==1):
+# 		return 0
+# 	else:
+# 		a=1+example(num-1)
+# 		print(a)
+# 		return a
+# example(11)
+# Recursion 
+
+# def a():
+#     num=[3,5,6,55,88,77]
+#     print(max(num))
+# a()
+
+# def perfect(a):
+# 	num=int(input("enter a number"))
+# 	i=1
+# 	sum=0
+# 	while i<num:
+# 		if num%i==0:
+# 			sum=sum+i
+# 		i=i+1
+# 	if num==sum:
+# 		print(num,"is a perfect number")
+# 	else:
+# 		print(num,"is a not perfect number")
+# perfect(a)
+
+# reverse = 0
+# def integer_reverse(number):
+#     global reverse
+     
+#     if(number > 0):
+#         Reminder = number % 10
+#         reverse = (reverse * 10) + Reminder
+#         integer_reverse(number // 10)
+#     return reverse
+ 
+ 
+# # number = int32452(input("Enter any Number: "))
+# #  
+# # rev = integer_reverse(number)
+ 
+# if(number == rev):
+#     print("The %d is a Palindrome Number" %number)
+# else:
+#     print("The %d is not a Palindrome Number" %number)
+
+
+def sum(a,b):
+#     add=a+b
+#     return add
+# def sub(c):
+#     p=sum(4,4)
+#     return p
+# print(sub(3))
+
+
+def greater(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+def sum (a,b,c):
+    if greater(a,b)>c:
+        return greater(a,b)
+    else:
+        return c
+print(sum(120,1166,19))
+
+
+
+
+# def function():
+#     i=1
+#     while i<=100:
+#         if i%3==0:
+#             print(i,"nav")
+#         if i%7==0:
+#             print(i,"gurukul")
+#         if i%21==0:
+#             print(i,"navgurukul")
+#         else:
+#             print(i)
+#         i+=1
+# function()
+
+# def ask-question():
+#     i=1
+#     while i<=100:
+#         print("pinku")
+# ask-question():
+
+
+# num=[8,2,3,0,7]
+# def my_number(numbers):
+# 	print(max(numbers))
+# my_number(num)
+
